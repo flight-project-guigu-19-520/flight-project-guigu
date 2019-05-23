af74131af3e80036733ec2968f178060c0bdb039
Ellie <1220130356@qq.com>
2019-05-23 20:11:00 +0800

    首页完成

--- js/navigation/navigation.js
+++ js/navigation/navigation.js
@@ -1,54 +1,41 @@
-(
-    function (w) {
+(function (w) {
         w.navigation=function () {
+            console.log(1)
             var key=document.querySelector(".key");
             var keyAgo=key.querySelector(".key_ago");
             var keyLater=key.querySelectorAll(".key_later");
-            var nav_background=document.querySelector(".nav_background");
-            var navItem=document.querySelectorAll(".nav-list .nav-item");
+            var navWrap = document.querySelector(".nav-wrap");
             var aItem = document.querySelectorAll(".item-anchor");
+            var navBackground=document.querySelector(".nav_background");
             var ago=true;
-
-            key.onclick=function () {
-
-                if (ago) {
-
-                    keyAgo.style.opacity = 0;
-                    keyLater[0].style.opacity = 1;
-                    keyLater[1].style.opacity = 1;
-                    nav_background.style.opacity = 1;
-                    nav_background.style.backgroundPositionY = "0";
-                    for (var i = 0; i < navItem.length; i++) {
-                        // navItem[i].style.animation="fadeInUp 1s linear "+i+"s" ;
-                        navItem[i].style.transition = "all 1s linear " + (1 + i / 5) + "s";
-                        navItem[i].style.opacity = 1;
-                    }
-                    ago = false;
-
-                } else {
+            key.onclick = function () {
+                if(ago){
+                    keyAgo.style.display="none";
+                    keyLater[0].style.display="inline-block";
+                    keyLater[1].style.display="inline-block";
+                    navWrap.style.top="0";
+                    navBackground.style.opacity="1";
+                    ago=false;
+                }else{
                     vanish();
-                    ago = true;
+                    ago=true;
                 }
             }
+
             for (var i = 0;i <aItem.length;i++) {
                 aItem[i].onclick=function () {
-                    console.log(111)
+                    console.log(111);
                     vanish();
                     ago = true;
                 }
             }
 
             function vanish() {
-                keyAgo.style.opacity=1;
-                keyLater[0].style.opacity=0;
-                keyLater[1].style.opacity=0;
-                nav_background.style.opacity=.2;
-                nav_background.style.backgroundPositionY="-980px";
-                for (var i = 0;i < navItem.length;i++){
-                    // navItem[i].style.animation="fadeOutDown 1s linear "+i+"s" ;
-                    navItem[i].style.transition="all 1s linear "+i/5+"s" ;
-                    navItem[i].style.opacity=0;
-                }
+                keyAgo.style.display="block ";
+                keyLater[0].style.display="none";
+                keyLater[1].style.display="none";
+                navWrap.style.top="-650px";
+                navBackground.style.opacity=".2";
             }
         }
     }
